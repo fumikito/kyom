a9d5ef280332ed8a5e30c8437419db7ad04b4c93
fumikito <guy@hametuha.com>
2020-03-19 15:58:37 +0900

    Try gooey effect.

--- singular.php
+++ singular.php
@@ -12,9 +12,14 @@ the_post();
 
 				<div class="entry-header-box uk-container">
 					<?php if ( has_post_thumbnail() ) : ?>
-					<div class="entry-header-thumbnail">
-						<?php the_post_thumbnail( 'big-block' ); ?>
-					</div>
+						<div class="entry-header-eyecatch entry-header-thumbnail">
+							<?php the_post_thumbnail( 'big-block' ); ?>
+						</div>
+					<?php else : ?>
+						<div class="entry-header-eyecatch entry-header-gooey" id="header-gooey">
+							<div class="gooey-wrapper">
+							</div>
+						</div>
 					<?php endif; ?>
 					<?php if ( $term = kyom_get_top_category() ) : ?>
 						<a class="entry-top-term" href="<?php echo get_term_link( $term ) ?>" rel="tag">

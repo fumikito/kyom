fca24e54288c617964a0e30f63ba3fd522909b73
fumikito <guy@hametuha.com>
2025-10-08 01:25:58 +0900

    RSSフィードが頻繁に更新されないように

--- footer.php
+++ footer.php
@@ -6,44 +6,58 @@
  */
 
 do_action( 'kyom_before_site_footer' );
-?>
-<footer class="site-footer">
+$cache = get_transient( 'kyom_footer' );
+if ( false !== $cache ) {
+	echo $cache;
+} else {
+	ob_start();
+	?>
+	<footer class="site-footer">
 
-	<div class="uk-container">
+		<div class="uk-container">
 
-		<?php if ( is_active_sidebar( 'footer-sidebar' ) ) : ?>
-		<div class="site-footer-widgets uk-grid-divider uk-grid" uk-grid>
-			<?php dynamic_sidebar( 'footer-sidebar' ); ?>
+			<?php if ( is_active_sidebar( 'footer-sidebar' ) ) : ?>
+				<div class="site-footer-widgets uk-grid-divider uk-grid" uk-grid>
+					<?php dynamic_sidebar( 'footer-sidebar' ); ?>
+				</div>
+			<?php endif ?>
+
+			<?php
+			if ( has_nav_menu( 'social-links' ) ) {
+				wp_nav_menu( [
+					'theme_location' => 'social-links',
+					'container' => 'nav',
+					'container_class' => 'site-footer-social',
+					'menu_class' => 'uk-iconnav',
+				] );
+			}
+
+			if ( has_nav_menu( 'bottom-pages' ) ) {
+				Kunoichi\SetMenu::nav_menu( [
+					'theme_location' => 'bottom-pages',
+					'depth' => 1,
+					'menu_class' => 'uk-subnav uk-subnav-divider',
+					'container' => 'nav',
+					'container_class' => 'kyom-bottom-nav',
+				] );
+			}
+			?>
+
+			<p class="site-footer-copy">
+				&copy; <?php echo esc_html( kyom_oldest_date() ); ?> <a href="<?php echo home_url(); ?>"
+																		rel="home"><?php bloginfo( 'name' ); ?></a>
+				| Powered by <a
+					href="https://wordpress.org">WordPress <?php echo esc_html( $GLOBALS[ 'wp_version' ] ); ?></a> and
+				<a href="https://github.com/fumikito/kyom">Kyom <?php echo kyom_version( true ); ?></a>.
+			</p>
 		</div>
-		<?php endif ?>
-
-		<?php
-		if ( has_nav_menu( 'social-links' ) ) {
-			wp_nav_menu( [
-				'theme_location'  => 'social-links',
-				'container'       => 'nav',
-				'container_class' => 'site-footer-social',
-				'menu_class'      => 'uk-iconnav',
-			] );
-		}
-
-		if ( has_nav_menu( 'bottom-pages' ) ) {
-			Kunoichi\SetMenu::nav_menu( [
-				'theme_location'  => 'bottom-pages',
-				'depth'           => 1,
-				'menu_class'      => 'uk-subnav uk-subnav-divider',
-				'container'       => 'nav',
-				'container_class' => 'kyom-bottom-nav',
-			] );
-		}
-		?>
-
-		<p class="site-footer-copy">
-			&copy; <?php echo esc_html( kyom_oldest_date() ); ?> <a href="<?php echo home_url(); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			| Powered by <a href="https://wordpress.org">WordPress <?php echo esc_html( $GLOBALS['wp_version'] ); ?></a> and <a href="https://github.com/fumikito/kyom">Kyom <?php echo kyom_version( true ); ?></a>.
-		</p>
-	</div>
-</footer>
-<?php do_action( 'wp_footer' ); ?>
+	</footer>
+	<?php
+	$footer = ob_get_contents();
+	ob_end_flush();
+	set_transient( 'kyom_footer', $footer, 36000 );
+}
+do_action( 'wp_footer' );
+?>
 </body>
 </html>

f65cf5633a8579a761b505bd4bbc69e0f7f57b27
fumikito <guy@hametuha.com>
2025-10-08 01:28:08 +0900

    構文エラー修正

--- footer.php
+++ footer.php
@@ -25,19 +25,19 @@ if ( false !== $cache ) {
 			<?php
 			if ( has_nav_menu( 'social-links' ) ) {
 				wp_nav_menu( [
-					'theme_location' => 'social-links',
-					'container' => 'nav',
+					'theme_location'  => 'social-links',
+					'container'       => 'nav',
 					'container_class' => 'site-footer-social',
-					'menu_class' => 'uk-iconnav',
+					'menu_class'      => 'uk-iconnav',
 				] );
 			}
 
 			if ( has_nav_menu( 'bottom-pages' ) ) {
 				Kunoichi\SetMenu::nav_menu( [
-					'theme_location' => 'bottom-pages',
-					'depth' => 1,
-					'menu_class' => 'uk-subnav uk-subnav-divider',
-					'container' => 'nav',
+					'theme_location'  => 'bottom-pages',
+					'depth'           => 1,
+					'menu_class'      => 'uk-subnav uk-subnav-divider',
+					'container'       => 'nav',
 					'container_class' => 'kyom-bottom-nav',
 				] );
 			}
@@ -47,7 +47,7 @@ if ( false !== $cache ) {
 				&copy; <?php echo esc_html( kyom_oldest_date() ); ?> <a href="<?php echo home_url(); ?>"
 																		rel="home"><?php bloginfo( 'name' ); ?></a>
 				| Powered by <a
-					href="https://wordpress.org">WordPress <?php echo esc_html( $GLOBALS[ 'wp_version' ] ); ?></a> and
+					href="https://wordpress.org">WordPress <?php echo esc_html( $GLOBALS['wp_version'] ); ?></a> and
 				<a href="https://github.com/fumikito/kyom">Kyom <?php echo kyom_version( true ); ?></a>.
 			</p>
 		</div>

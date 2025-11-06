cf5238e9aeb3cee675a6af74c5217372bf8b5c56
fumikito <guy@hametuha.com>
2025-11-06 18:20:14 +0900

    #60 構文エラーとアクションを修正

--- src/js/block-variations.js
+++ src/js/block-variations.js
@@ -4,20 +4,21 @@
  * @package kyom
  */
 
-wp.domReady(() => {
+wp.domReady( () => {
+
 	// Netabareブロック（Groupブロックのバリエーション）
-	wp.blocks.registerBlockVariation('core/group', {
+	wp.blocks.registerBlockVariation( 'core/group', {
 		name: 'netabare',
 		title: 'ネタバレ',
 		description: 'クリックするまで内容がぼかされて表示されるブロックです。',
 		category: 'kyom',
 		icon: 'hidden',
 		attributes: {
-			className: 'netabare',
+			className: 'netabare'
 		},
 		innerBlocks: [
-			['core/paragraph', {placeholder: 'ネタバレの内容を入力...'}],
+			[ 'core/paragraph', {placeholder: 'ネタバレの内容を入力...'} ]
 		],
-		scope: ['inserter'],
+		scope: [ 'inserter' ]
 	});
 });
